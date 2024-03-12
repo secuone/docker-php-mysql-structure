96d9789a042c895e2d9ff44f3c9389af83d3a3b6
Alexandru Raul Cenan <alexandru.cenan@scalefast.com>
2024-03-12 16:59:46 +0100

    chore: add migration example

---
+++ migrations/database.sql
@@ -0,0 +1,14 @@
+-- CREATE DATABASE IF NOT EXISTS `example`;
+
+-- USE example;
+
+-- CREATE TABLE IF NOT EXISTS `users`
+-- (
+-- 	`id` INT(10) PRIMARY KEY AUTO_INCREMENT,
+-- 	`name` VARCHAR(255) NOT NULL,
+-- 	`lastname` VARCHAR(255) NOT NULL
+-- );
+
+-- INSERT INTO users (id, name, lastname) VALUES (1, "Alejandro", "Campos");
+-- INSERT INTO users (id, name, lastname) VALUES (2, "Javier", "Montes");
+-- INSERT INTO users (id, name, lastname) VALUES (3, "Pedro", "Gómez");
